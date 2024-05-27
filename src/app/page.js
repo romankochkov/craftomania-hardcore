80b0dbae8b35a76a173ec4d7a567ac5dc3e60a84
romankochkov <roman.kochkov@hotmail.com>
2024-05-27 00:00:06 -0400

    RE

--- src/app/page.js
+++ src/app/page.js
@@ -5,61 +5,10 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <nav>
+      <section className={styles.welcome}>
         <div className="container">
-          <div className={styles.navbar}>
-            <div className="d-flex justify-content-between">
-              <div className={styles.logo}>
-                <a href="/">Craftomania</a>
-                <br className="d-block d-sm-none"></br>
-                <span className={`${styles.separator} d-none d-sm-inline-flex`}>｜</span>
-                <span className={styles.hardcore}>HARDCORE</span>
-              </div>
-              <div>
-                <div className="d-flex justify-content-end">
-                  <div className={styles.link}>Вики</div>
-                  <div className={styles.link}>Донат</div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </nav>
-
-      <section className={styles.section1}>
-        <div className="container">
-          <div className="row">
-            <div className="col-9">
-              <div className={styles.start}>
-                <Image
-                  src="/wallpaper.png"
-                  fill={true}
-                  quality={100}
-                  alt="Wallpaper"
-                  className={styles.wallpaper} />
-                <div className={styles.info}>
-                  <div className="d-flex justify-content-between">
-                    <div className={styles.ip}>
-                      mc.craftomania.net
-                    </div>
-                    <div>
-                      <div className={styles.phase}>
-                        ФАЗА III
-                      </div>
-                      <div className={styles.date}>
-                        ВАЙП ЧЕРЕЗ 2 ДНЯ
-                      </div>
-                      
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="col-3">
-              <div className={styles.rating}>
-
-              </div>
-            </div>
+          <div>
+            <div className=""></div>
           </div>
         </div>
       </section>
